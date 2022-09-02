89b5e98983c3787597814ffec1b7b8c34487d326
Elizaveta9988 <109989600+Elizaveta9988@users.noreply.github.com>
2022-09-02 16:42:01 +0300

    Add files via upload

---
+++ Семенар/Task со звездочкой/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите функцию, которая принимает одно число - высоту елочки и рисует ее в консоли звездочками.
+using static System.Console;
+class ChristmasTree {
+    static void Main() {
+        int size;
+        Write("Enter size: ");
+        while (!int.TryParse(ReadLine(), out size) || size <= 0)
+            Write("Enter size, damn it: ");
+        for (int i = 1; i <= size; i++)
+            for (int j = 1; j <= i; j++)
+                WriteLine(new string(' ', size - j) + new string('*', j * 2 - 1));
+    }
+}
